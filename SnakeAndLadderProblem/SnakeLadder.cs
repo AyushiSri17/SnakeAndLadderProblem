5d1c81147193cd7451523f3152ac6ed3b2eeeab9
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-01-25 17:19:28 +0530

    [Ayushi] Added Rolling Of Die

--- SnakeAndLadderProblem/SnakeLadder.cs
+++ SnakeAndLadderProblem/SnakeLadder.cs
@@ -10,9 +10,14 @@ namespace SnakeAndLadderProblem
     {
         public void Game()
         {
+            //UC1-Start Position
             int start = 0;
             Console.WriteLine("Start Position:" + start);
-            
+
+            //Rolling a die
+            Random random = new Random();
+            int roll = random.Next(1, 7);
+            Console.WriteLine("Die output is: "+roll);
         }
     }
 }
