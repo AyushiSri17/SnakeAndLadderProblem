fd6d612c95384bf2507b59a1405964d9b35c60e1
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-01-25 18:24:51 +0530

    [Ayushi] Added Check for player position

--- SnakeAndLadderProblem/SnakeLadder.cs
+++ SnakeAndLadderProblem/SnakeLadder.cs
@@ -10,7 +10,7 @@ namespace SnakeAndLadderProblem
     {
         public void Game()
         {
-            //UC1-Start Position
+            //Start Position
             int start = 0;
             Console.WriteLine("Start Position:" + start);
 
@@ -18,6 +18,28 @@ namespace SnakeAndLadderProblem
             Random random = new Random();
             int roll = random.Next(1, 7);
             Console.WriteLine("Die output is: "+roll);
+
+            //Check For Position
+            int playerPosition = 0;
+            int ladder = 1;
+            int snake = 2;
+            Console.WriteLine("1:ladder\t 2: snake\t 3: noplay");
+            int option = random.Next(1, 4);
+            Console.WriteLine("Choosed option is: " + option);
+            if (option == ladder)
+            {
+                playerPosition = playerPosition + roll;
+                Console.WriteLine("With ladder player moves {0} steps ahead and now on {1} position ",roll,playerPosition);
+            }
+            else if (option == snake)
+            {
+                playerPosition = playerPosition + roll;
+                Console.WriteLine("With snake player moves {0} steps back and now is on {1} position ",roll,playerPosition);
+            }
+            else
+            {
+                Console.WriteLine("no play player stays on its position only no movement " + playerPosition);
+            }
         }
     }
 }
