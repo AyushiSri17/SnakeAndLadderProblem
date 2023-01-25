155f28ccb3b1be338d117f9d877ffbae02015212
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-01-25 17:00:43 +0530

    [Ayushi] Added Start Position

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -11,6 +11,9 @@ namespace SnakeAndLadderProblem
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Snake And Ladder Problem");
+            //Object creation of SnakeLadder class
+            SnakeLadder snakeLadder = new SnakeLadder();
+            snakeLadder.Game();
             Console.ReadLine();
         }
     }
---
+++ SnakeAndLadderProblem/SnakeLadder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class SnakeLadder
+    {
+        public void Game()
+        {
+            int start = 0;
+            Console.WriteLine("Start Position:" + start);
+            
+        }
+    }
+}
