d78305bcd7684fe03d78ee581e0e79e7a8227fda
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-01-28 01:11:48 +0530

    [Ayushi] Added Two Player game with repeat Ladder condition

--- SnakeAndLadderProblem/SnakeLadder.cs
+++ SnakeAndLadderProblem/SnakeLadder.cs
@@ -11,17 +11,20 @@ namespace SnakeAndLadderProblem
         public void Game()
         {
             //Start Position
-            int playerPosition = 0;
+            int player_1_Position = 0;
+            int player_2_Position = 0;
             int start = 0;
             Console.WriteLine("Start Position:" + start);
             int dieCount=0;
+            //Here value of i will decide which player's turn is to play
+            int i = 2;
+            Random random = new Random();
 
-            while (playerPosition < 100)
+            while (player_1_Position < 100 && player_2_Position<100)
             {
-                //Rolling a die
-                Random random = new Random();
+                //Rolling a die 
                 int roll = random.Next(1, 7);
-                Console.WriteLine("Die output is: " + roll);
+                Console.WriteLine("\nDie output is: " + roll);
 
                 //Check For Position         
                 const int Ladder = 1;
@@ -30,38 +33,85 @@ namespace SnakeAndLadderProblem
                 Console.WriteLine("1:ladder\t 2: snake\t 3: noplay");
                 int option = random.Next(1, 4);
                 Console.WriteLine("Choosed option is: " + option);
-                if(playerPosition>=0)
-                { 
-                    switch(option)
+                Console.WriteLine("i = " + i);
+
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Player 1 turn to play");
+                    if (player_1_Position >= 0)
                     {
-                        case Ladder:                
-                            playerPosition = playerPosition + roll;
-                            Console.WriteLine("With ladder player moves {0} steps ahead and now on {1} position ", roll, playerPosition);
-                            if(playerPosition>100)
-                            {
-                                Console.WriteLine("Player needs to stay on its previous position only");
-                                playerPosition = playerPosition - roll;
-                                Console.WriteLine("Player is still on its previous position that is {0} position", playerPosition);
-                            }
-                            else if(playerPosition==100)
-                            {
-                                Console.WriteLine("Player won");
-                            }
-                            break;
-                        case Snake:                
-                            playerPosition = playerPosition - roll;
-                            Console.WriteLine("With snake player moves {0} steps back and now is on {1} position ", roll, playerPosition);
-                            break;
-                        default:               
-                            Console.WriteLine("no play player stays on its position only no movement " + playerPosition);
-                            break;
+                        switch (option)
+                        {
+                            case Ladder:
+                                player_1_Position = player_1_Position + roll;
+                                Console.WriteLine("With ladder player moves {0} steps ahead and now on {1} position ", roll, player_1_Position);
+                                i--;
+                                Console.WriteLine("Its a Ladder player 1 will play again");
+                                if (player_1_Position > 100)
+                                {
+                                    Console.WriteLine("Player needs to stay on its previous position only");
+                                    player_1_Position = player_1_Position - roll;
+                                    Console.WriteLine("Player is still on its previous position that is {0} position", player_1_Position);
+                                }
+                                else if (player_1_Position == 100)
+                                {
+                                    Console.WriteLine("Player 1 won");
+                                }
+                                break;
+                            case Snake:
+                                player_1_Position = player_1_Position - roll;
+                                Console.WriteLine("With snake player moves {0} steps back and now is on {1} position ", roll, player_1_Position);
+                                break;
+                            default:
+                                Console.WriteLine("no play player stays on its position only no movement " + player_1_Position);
+                                break;
+                        }
+                    }
+                    else if (player_1_Position < 0)
+                    {
+                        Console.WriteLine("though player goes into negative position then player needs to play from start position again");
+                        player_1_Position = start;
                     }
                 }
-                else if(playerPosition<0)
+                else
                 {
-                    Console.WriteLine("though player goes into negative position then player needs to play from start position again");
-                    playerPosition = start;
+                    Console.WriteLine("Player 2 turn to play");
+                    if (player_2_Position >= 0)
+                    {
+                        switch (option)
+                        {
+                            case Ladder:
+                                player_2_Position = player_2_Position + roll;
+                                Console.WriteLine("With ladder player moves {0} steps ahead and now on {1} position ", roll, player_2_Position);
+                                i--;
+                                Console.WriteLine("Its a Ladder player 2 will play again");
+                                if (player_2_Position > 100)
+                                {
+                                    Console.WriteLine("Player needs to stay on its previous position only");
+                                    player_2_Position = player_2_Position - roll;
+                                    Console.WriteLine("Player is still on its previous position that is {0} position", player_2_Position);
+                                }
+                                else if (player_2_Position == 100)
+                                {
+                                    Console.WriteLine("Player 2 won");
+                                }
+                                break;
+                            case Snake:
+                                player_2_Position = player_2_Position - roll;
+                                Console.WriteLine("With snake player moves {0} steps back and now is on {1} position ", roll, player_2_Position);
+                                break;
+                            default:
+                                Console.WriteLine("no play player stays on its position only no movement " + player_2_Position);
+                                break;
+                        }
+                    }
+                    else if (player_2_Position < 0)
+                    {
+                        Console.WriteLine("though player goes into negative position then player needs to play from start position again");
+                        player_2_Position = start;
+                    }
                 }
+                i++;
                 dieCount++;   
             }
             Console.WriteLine("\n Total number of times die rolled is: "+dieCount );
